768b3853b84c4273be49d16a72a923264d86f4f2
ariaru <ariaru@hotmail.de>
2016-08-17 22:10:07 +0200

    stand-in validation, saving&routing stuff

--- src/clj/workshop_manager/routes/home.clj
+++ src/clj/workshop_manager/routes/home.clj
@@ -1,17 +1,40 @@
 (ns workshop-manager.routes.home
   (:require [workshop-manager.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [workshop-manager.db.core :as db]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]
+            [ring.util.response :refer [redirect]]))
 
-(defn home-page []
+(defn validate-attendee [params]
+	(first
+		(b/validate
+			params
+			:email v/required)))
+
+(defn save-attendee! [{:keys [params]}]
+  (if-let [errors (validate-attendee params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-attendee!
+      ;; This ^ method has to be added to src/clj/workshop_manager/db/core.clj
+       (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
+
+(defn home-page [{:keys [flash]}]
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html"
+    (merge {:attendees (db/get-attendees)}
+    				(select-keys flash [:email :errors]))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
+  (POST "/" request (save-attendee! request))
   (GET "/about" [] (about-page)))
 
